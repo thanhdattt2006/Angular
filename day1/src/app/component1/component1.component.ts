6e4b14425e0c372b9d8c8514edfb1c13db936f0e
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-06-08 10:28:13 +0700

    update data binding example in component1 and turn off strict mode

--- day1/src/app/component1/component1.component.ts
+++ day1/src/app/component1/component1.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './component1.component.html'
 })
 
-export class Component1Component {
+export class Component1Component implements OnInit {
 
+  constructor() { }
+
+  id: number = 123;
+  username: string;
+  status: boolean;
+  phone: any;
+
+  ngOnInit() {
+    this.username = "Dave"
+    this.status = true;
+  }
 }
