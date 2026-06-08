edf32035ac8a8d489f790e0d87468f5e1e6a06ce
thanhdattt2006 <thanhdattt2006@gmail.com>
2026-06-08 10:54:54 +0700

    update component1 with property binding, multiple css files and calculated properties

--- day1/src/app/component1/component1.component.ts
+++ day1/src/app/component1/component1.component.ts
@@ -2,20 +2,31 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './component1.component.html'
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css', './component2.component.css']
 })
 
 export class Component1Component implements OnInit {
 
   constructor() { }
 
-  id: number = 123;
+  id: number = 1;
   username: string;
   status: boolean;
   phone: any;
+  quantity: number;
+  price: number;
+  total: number;
+  photo: string;
+  height: number;
 
   ngOnInit() {
     this.username = "Dave"
     this.status = true;
+    this.quantity = 10;
+    this.price = 8;
+    this.total = this.quantity * this.price;
+    this.photo = "CV.jpg";
+    this.height = 100;
   }
 }
